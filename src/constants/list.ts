0cf63fad6840151a9772d7101e667c13cee698c2
John <bioj@hotmail.fr>
2022-06-21 20:52:11 +0900

    AMM: new address tokens

--- src/constants/list.ts
+++ src/constants/list.ts
@@ -10,7 +10,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
         "name": "GW3S Dai Stablecoin",
-        "address": "0x9E3bcc9CB64f808cD9F771978D591a15625625E6",
+        "address": "0x6899A3f3120f1931EA7E6eECAa39C06cE6AbD0B7",
         "symbol": "DAI",
         "decimals": 18,
         "chainId": 80001,
@@ -18,7 +18,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
         "name": "GW3S Aavegotchi GHST Token",
-        "address": "0x8086A8fC26e6305bAD05E9347fb14c993c840814",
+        "address": "0xd9f1F89Ee60F1D1B7E78F11641C1B1b7dAED2b39",
         "symbol": "GHST",
         "decimals": 18,
         "chainId": 80001,
@@ -26,7 +26,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
             "name": "GW3S Aavegotchi FUD",
-            "address": "0x5619C0127CFF69FA1c371B81CcD7BC19423AB886",
+            "address": "0xB0931B18B42B7F47309Eeba8510345E0f76E75A5",
             "symbol": "FUD",
             "decimals": 18,
             "chainId": 80001,
@@ -34,7 +34,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
             "name": "GW3S  Aavegotchi FOMO",
-            "address": "0x084a0dD72D769d6f015590D29D125f0884DefC41",
+            "address": "0x46Ef0438a11B19F862AD620a4E3f56b43aEC7D2F",
             "symbol": "FOMO",
             "decimals": 18,
             "chainId": 80001,
@@ -42,7 +42,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
             "name": "GW3S Aavegotchi ALPHA",
-            "address": "0xC9258d7c67eEbD9490f352B04E211fD2D8999e2c",
+            "address": "0x7c57071637AEd48D1Ba6c314d02292c191e9bBb1",
             "symbol": "ALPHA",
             "decimals": 18,
             "chainId": 80001,
@@ -50,7 +50,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
             "name": "GW3S Aavegotchi KEK",
-            "address": "0x08B3933C662B1ea64f8A1B0465ceBEbb53C4d564",
+            "address": "0x0CD99CcA64ba23C41a721904dbED434507E49bE3",
             "symbol": "KEK",
             "decimals": 18,
             "chainId": 80001,
@@ -58,7 +58,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
         "name": "GW3S Aave",
-        "address": "0x240c7dc7C2Fb064f0852289F0b756a46FA0fc335",
+        "address": "0xa32335BE3aC8404F8f0C154de3973300c3B4F72C",
         "symbol": "AAVE",
         "decimals": 18,
         "chainId": 80001,
@@ -66,7 +66,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
         "name": "GW3S  Uniswap",
-        "address": "0xAb11F2308db8ac43703A4d1846c56030E6b61AE1",
+        "address": "0xf53360819aCc346edaaa162B7d9bde1f94F75e73",
         "symbol": "UNI",
         "decimals": 18,
         "chainId": 80001,
@@ -74,7 +74,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
         "name": "GW3S  USD Coin",
-        "address": "0x1080E133779343e7Cb90bB94a750A7067304791A",
+        "address": "0xEA1108D014C4c3f50AA398399740Dc42345859BE",
         "symbol": "USDC",
         "decimals": 18,
         "chainId": 80001,
@@ -82,7 +82,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
         "name": "GW3S Ether",
-        "address": "0x71D6Ec51a5FB485A6Cdf67dC6D3f1e92D6b89bf1",
+        "address": "0xb91508934d655f54dC1D73F38b23d43d6Db99D8b",
         "symbol": "ETH",
         "decimals": 18,
         "chainId": 80001,
@@ -90,7 +90,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
         "name": "GW3S Tether USD",
-        "address": "0x5D116F9aAe59b7c35be7a377E7140fb7C337F285",
+        "address": "0x046787F8470e4063f2658D82536521b8E50627A1",
         "symbol": "USDT",
         "decimals": 18,
         "chainId": 80001,
@@ -98,7 +98,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
             "name": "GW3S Wrapped Bitcoin",
-            "address": "0xa5F7fBF20922185aDcF07c68e30994959e0CB361",
+            "address": "0xa09286a5a2b788baAd6585bbd38aBDA120e61A20",
             "symbol": "WBTC",
             "decimals": 18,
             "chainId": 80001,
@@ -106,7 +106,7 @@ export const DEFAULT_TOKEN_LIST_URL = {
         },
         {
             "name": "ChainLink Token",
-            "address": "0x6E074085f4f3030cfb0D4E29789233Dac84a2a3B",
+            "address": "0x4327fB634A82BdB310De0F0FDac2c937Dcc82127",
             "symbol": "LINK",
             "decimals": 18,
             "chainId": 80001,
