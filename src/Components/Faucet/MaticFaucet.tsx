f10a68490d39afee822f4d7dcd64209458a4f03b
John <bioj@hotmail.fr>
2022-06-24 13:18:45 +0900

    typo

--- src/Components/Faucet/MaticFaucet.tsx
+++ src/Components/Faucet/MaticFaucet.tsx
@@ -8,7 +8,7 @@ const MaticFaucet: React.FC = () => {
                 <Center m={'0.5rem'}>
                 <Button as='a' href="https://mumbaifaucet.com/" target="_blank">Get 1 MATIC</Button>
                 </Center>
-                <Text fontSize={"sm"} fontStyle="italic">Since transaction requiert to pay gas fees you will need some MATIC token in your wallet to interact with them.</Text>
+                <Text fontSize={"sm"} fontStyle="italic">Since transactions requiert to pay gas fees you will need some MATIC token in your wallet to send them.</Text>
             </Container>
         </Box>
    )
