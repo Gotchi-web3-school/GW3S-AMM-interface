23699cfb6a9e64c1d04f69c02e385c75947fa1ed
John <bioj@hotmail.fr>
2022-06-24 16:41:51 +0900

    AMM/PoolShare: fix the front

--- src/Components/AMM/Pools/Pool/PoolData.tsx
+++ src/Components/AMM/Pools/Pool/PoolData.tsx
@@ -37,20 +37,20 @@ const PoolData: React.FC<{pool: IPool, setState: React.Dispatch<string>}> = ({po
             <Text textAlign={"left"}>Your LP tokens:</Text>
             <Flex justifyContent={"right"}>
                 <Text position={"relative"} left="12px" textAlign={"right"} fontWeight="bold">{pool?.lpToken.balance?.toFixed(2) ?? "0"}</Text>
-                {tokenA.logo ? <Image zIndex={1} position={"relative"} left="20px" borderRadius='full' boxSize='20px' src={tokenA.logo} alt={pair.token0.name}/> : <QuestionOutlineIcon />}
-                {tokenB.logo  ? <Image position={"relative"} left="10px" borderRadius='full' boxSize='20px' src={tokenB.logo} alt={pair.token1.name}/> : <QuestionOutlineIcon />}
+                {tokenA.logo ? <Image zIndex={1} position={"relative"} left="20px" borderRadius='full' boxSize='20px' src={tokenA.logo} alt={pair.token0.name}/> : <QuestionOutlineIcon zIndex={1} position={"relative"} left="15px"/>}
+                {tokenB.logo  ? <Image position={"relative"} left="10px" borderRadius='full' boxSize='20px' src={tokenB.logo} alt={pair.token1.name}/> : <QuestionOutlineIcon position={"relative"} left="10px"/>}
             </Flex>
 
             <Text textAlign={"left"}>Pooled tokenA:</Text>
             <Flex justifyContent={"right"}>
                 <Text textAlign={"right"} fontWeight="bold">{pool?.tokenA?.pooled.toSignificant(3) ?? "0"}</Text>
-                {tokenA.logo ? <Image ml="2" display={"initial"} borderRadius='full' boxSize='20px' src={tokenA.logo} alt={pair.token0.name}/> : <QuestionOutlineIcon />}
+                {tokenA.logo ? <Image ml="2" display={"initial"} borderRadius='full' boxSize='20px' src={tokenA.logo} alt={pair.token0.name}/> : <QuestionOutlineIcon ml="2"/>}
             </Flex>
 
             <Text textAlign={"left"}>Pooled tokenB:</Text>
             <Flex justifyContent={"right"}>
                 <Text textAlign={"right"} fontWeight="bold">{pool?.tokenB?.pooled.toSignificant(3) ?? "0"}</Text>
-                {tokenB.logo  ? <Image ml="2" display={"initial"} borderRadius='full' boxSize='20px' src={tokenB.logo} alt={pair.token1.name}/> : <QuestionOutlineIcon />}
+                {tokenB.logo  ? <Image ml="2" display={"initial"} borderRadius='full' boxSize='20px' src={tokenB.logo} alt={pair.token1.name}/> : <QuestionOutlineIcon ml="2"/>}
             </Flex>
 
             <Text textAlign={"left"} >Your pool share</Text><Text textAlign={"right"} fontWeight="bold">{pool?.lpToken.share.toSignificant(2) ?? "0"}%</Text>
