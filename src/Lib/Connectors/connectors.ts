dfd65ea1cb80e7c9e3d2b3243565f5d6c795f839
John <bioj@hotmail.fr>
2022-06-26 19:56:13 +0900

    AMM switch network

--- src/Lib/Connectors/connectors.ts
+++ src/Lib/Connectors/connectors.ts
@@ -1,5 +1,5 @@
 import {InjectedConnector} from '@web3-react/injected-connector';
-import { ethers } from 'ethers';
+import { utils } from 'ethers';
 
 export const injected = new InjectedConnector({supportedChainIds: [1, 4, 10, 58, 137, 1284, 1285, 42161, 43114, 80001]});
 
@@ -7,27 +7,32 @@ export const connectUser = (activate: any): void => {
     activate(injected)
 }
 
+
+const networkMap = {
+  POLYGON_MAINNET: {
+    chainId: utils.hexValue(137), // '0x89'
+    chainName: "Matic(Polygon) Mainnet", 
+    nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
+    rpcUrls: ["https://polygon-rpc.com"],
+    blockExplorerUrls: ["https://www.polygonscan.com/"],
+  },
+  MUMBAI_TESTNET: {
+    chainId: utils.hexValue(80001), // '0x13881'
+    chainName: "Matic(Polygon) Mumbai Testnet",
+    nativeCurrency: { name: "tMATIC", symbol: "tMATIC", decimals: 18 },
+    rpcUrls: ["https://rpc-mumbai.maticvigil.com"],
+    blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+  },
+};
+
+
 export const switchNetwork = async(window: any, chainId: number) => {
     try {
         await window.ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: ethers.utils.hexValue(chainId) }]
+          method: "wallet_addEthereumChain",
+          params: [networkMap.MUMBAI_TESTNET],
         });
-        } catch (err: any) {
+    } catch (err: any) {
           // This error code indicates that the chain has not been added to MetaMask
-          await window.ethereum.request({
-            method: 'wallet_addEthereumChain',
-            params: [
-              {
-                chainName: 'Mumbai Testnet',
-                iconUrls: ["https://seeklogo.com/images/P/polygon-matic-logo-86F4D6D773-seeklogo.com.png"],
-                chainId: ethers.utils.hexValue(chainId),
-                nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'TMATIC' },
-                rpcUrls: ["https://rpc-mumbai.maticvigil.com", 'https://rpc-mumbai.matic.today'],
-                blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
-                chain: "testnet",
-              }
-            ]
-          });
-      }
+    }
 }
