6c4f1f6431823c6be7076ea682ab7f1f3ee85e06
John <bioj@hotmail.fr>
2022-08-01 03:37:43 +0900

    implemented Router for levels

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
 import { useContext } from "react"
-import { Box, useColorModeValue, Grid, GridItem } from "@chakra-ui/react"
-import Navbar from "./Components/Navbar/Navbar"
-import AMM from "./Components/AMM"
-import Erc20Generator from "./Components/Erc20Generator"
-import MaticFaucet from "./Components/Faucet/MaticFaucet"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Box, useColorModeValue } from "@chakra-ui/react"
 import { GeneralContext } from "./Provider/GeneralProvider"
+import Navbar from "./Components/Navbar/Navbar"
+import MaticFaucet from "./Components/Faucet/MaticFaucet"
+import LevelPills from "./Components/Levels/LevelPills"
+import Playground from "./Pages/Playground"
 
 const App: React.FC = () => {
   const {balance} = useContext(GeneralContext)
   
    return (
     <Box
-      bgGradient={useColorModeValue(
-        "linear-gradient(to-br, rgba(235,235,235,1) 0%, rgba(179,200,255,1) 100%)",
+     bgGradient={useColorModeValue(
+       "linear-gradient(to-br, rgba(235,235,235,1) 0%, rgba(179,200,255,1) 100%)",
         "linear(gray.800, purple.900, purple.800, purple.900, gray.800)"
       )}>
-      <Navbar />
-      {parseFloat(balance) < 0.01 && <MaticFaucet />}
-      <Grid 
-      mt="6rem"
-      templateRows='repeat(2, 1fr)'
-      templateColumns='repeat(6, 1fr)'
-      gap={4}
-      >
-        <GridItem rowSpan={2} colSpan={1}>
-          <Erc20Generator />
-        </GridItem>
-        <GridItem colStart={3}>
-          <AMM />
-        </GridItem>
-      </Grid>
+      <Router>
+        <Navbar />
+        <LevelPills />
+        {parseFloat(balance) < 0.01 && <MaticFaucet />}
+        <Routes>
+          <Route path="/" element={<Playground />} />
+          <Route path="/level/1" element={<Playground />} />
+        </Routes>
+      </Router>
     </Box>
    )
 }
