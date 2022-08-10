adf67802b1d0e581fb3b893b414ee52db63bfafa
John <bioj@hotmail.fr>
2022-08-10 18:03:16 +0900

    level1: DONE

--- src/Components/Levels/Level1.tsx
+++ src/Components/Levels/Level1.tsx
@@ -1,7 +1,54 @@
+import { useContext, useEffect } from "react"
+import { useWeb3React } from "@web3-react/core"
+import { Box, Spacer, Stack, Image, Text, useToast, Button, Center } from "@chakra-ui/react"
+import { levels } from "../../Constants/levels"
+import { ContractContext } from "../../Provider/ContractProvider"
+import { claim_l1 } from "../../Lib/Smart-contracts/Levels/level1Facet"
+import { LevelContext } from "../../Provider/LevelProvider"
+import { fetchLevelState } from "../../Lib/Smart-contracts/Levels"
+import Card from "./Card"
+const lockedChest = require("../../Assets/chests/lockedChest.png")
+const closeChest = require("../../Assets/chests/closedChest.png")
+const opennedChest = require("../../Assets/chests/opennedChest.png")
+
 const Level1: React.FC = () => {
+    const signer = useWeb3React()
+    const {ILevel1Facet, LevelLoupeFacet} = useContext(ContractContext)
+    const {hasClaimed, hasCompleted} = useContext(LevelContext)
+    const {dispatch} = useContext(LevelContext)
+    const toast = useToast()
+    const message = " I hereby confirm that i will pursue the challenges ahead and be devoted to protect the Gotchiverse against the baad lickuidators"
+
+    useEffect(() => {
+        try {
+            fetchLevelState(LevelLoupeFacet!, signer, 1).then(result => {
+                dispatch({type: "SET_LEVEL_STATE", payload: result})
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }, [LevelLoupeFacet, signer, dispatch])
+    
     return (
-    <>
-    </>
+    <Box margin={"auto"}>
+        <Stack direction={"row"} m="5rem" align="center">
+            <Spacer />
+                <Box margin="auto" maxW="25%">
+                    <Stack spacing="5">
+                        <Text textAlign={"center"}>{message}</Text>
+                        <Center>
+                            <Button margin="auto" maxW="25%">Sign</Button>
+                        </Center>
+                    </Stack>
+                </Box>
+            <Spacer />
+            <Box as="button" display={"flex"} margin="auto" onClick={() => claim_l1({Facet: ILevel1Facet, toast: toast})}>
+                    <Image src={hasCompleted ? hasClaimed ? opennedChest : closeChest : lockedChest}/>
+                </Box>
+            <Spacer />
+            <Card level={levels[1]}/>
+        </Stack>
+    </Box>
     )
 }
 
--- src/Lib/Smart-contracts/Levels/level1Facet.ts
+++ src/Lib/Smart-contracts/Levels/level1Facet.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 
 interface ClaimTx {
-    ILevel1: ethers.Contract | undefined,
+    Facet: ethers.Contract | undefined,
     toast: any,
 }
 
@@ -9,17 +9,17 @@ export const claim_l1 = async(tx: ClaimTx) => {
     try {    
         console.warn("CLAIM")
         console.log("///////////////////////////////////////////////")
-        console.log("Level: " + 0)
+        console.log("Level: " + 1)
         console.log("///////////////////////////////////////////////")
     
         //Estimation of the gas cost
-        const gas = await tx.ILevel1?.estimateGas.claim_l1() 
+        const gas = await tx.Facet?.estimateGas.claim_l1() 
         console.log("Gas cost: " + (ethers.utils.formatEther(gas?.toString() ?? "") + " MATIC"))
         
-        const transaction = await tx.ILevel1?.claim_l1()
+        const transaction = await tx.Facet?.claim_l1()
     
         tx.toast({
-            title: `Claim level 0`,
+            title: `Claim level 1`,
             description: `transaction pending at: ${transaction?.hash}`,
             position: "top-right",
             status: "warning",
@@ -29,7 +29,7 @@ export const claim_l1 = async(tx: ClaimTx) => {
         const receipt = await transaction.wait()
     
         tx.toast({
-            title: `Claim level 0`,
+            title: `Claim level 1`,
             description: `Reward claimed successfully`,
             position: "top-right",
             status: "success",
@@ -42,11 +42,15 @@ export const claim_l1 = async(tx: ClaimTx) => {
         console.log(error.error)
         tx.toast({
             position: "bottom-right",
-            title: 'Claim level 0.',
+            title: 'Claim level 1.',
             description: `${error.error.message}`,
             status: 'error',
             duration: 9000,
             isClosable: true,
           })
     }
+}
+
+export const signMessage = (signer: any, message: string) => {
+    
 }
