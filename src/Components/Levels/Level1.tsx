ebf8c46897c12847b94cf14447eaf0f6c9d0fa2b
John <bioj@hotmail.fr>
2022-08-10 21:55:19 +0900

    level1: DONE

--- src/Components/Levels/Level1.tsx
+++ src/Components/Levels/Level1.tsx
@@ -3,21 +3,34 @@ import { useWeb3React } from "@web3-react/core"
 import { Box, Spacer, Stack, Image, Text, useToast, Button, Center } from "@chakra-ui/react"
 import { levels } from "../../Constants/levels"
 import { ContractContext } from "../../Provider/ContractProvider"
-import { claim_l1 } from "../../Lib/Smart-contracts/Levels/level1Facet"
+import { claim_l1, CompleteTx } from "../../Lib/Smart-contracts/Levels/level1Facet"
 import { LevelContext } from "../../Provider/LevelProvider"
 import { fetchLevelState } from "../../Lib/Smart-contracts/Levels"
 import Card from "./Card"
+import { completes } from "../../Lib/Smart-contracts/Levels"
+
 const lockedChest = require("../../Assets/chests/lockedChest.png")
 const closeChest = require("../../Assets/chests/closedChest.png")
 const opennedChest = require("../../Assets/chests/opennedChest.png")
 
 const Level1: React.FC = () => {
     const signer = useWeb3React()
-    const {ILevel1Facet, LevelLoupeFacet} = useContext(ContractContext)
+    const {ILevel1Facet, LevelLoupeFacet, LevelFacets} = useContext(ContractContext)
     const {hasClaimed, hasCompleted} = useContext(LevelContext)
     const {dispatch} = useContext(LevelContext)
     const toast = useToast()
     const message = " I hereby confirm that i will pursue the challenges ahead and be devoted to protect the Gotchiverse against the baad lickuidators"
+
+    const complete = () => {
+        const tx: CompleteTx = {
+            signer: signer,
+            Facet: LevelFacets[1],
+            LoupeFacet: LevelLoupeFacet,
+            toast: toast,
+            dispatch: dispatch
+        }
+        completes[1]!(tx)
+    }
 
     useEffect(() => {
         try {
@@ -28,6 +41,7 @@ const Level1: React.FC = () => {
             console.log(error)
         }
     }, [LevelLoupeFacet, signer, dispatch])
+    console.log(hasCompleted)
     
     return (
     <Box margin={"auto"}>
@@ -37,13 +51,13 @@ const Level1: React.FC = () => {
                     <Stack spacing="5">
                         <Text textAlign={"center"}>{message}</Text>
                         <Center>
-                            <Button margin="auto" maxW="25%">Sign</Button>
+                            <Button margin="auto" maxW="25%" onClick={complete}>Sign</Button>
                         </Center>
                     </Stack>
                 </Box>
             <Spacer />
             <Box as="button" display={"flex"} margin="auto" onClick={() => claim_l1({Facet: ILevel1Facet, toast: toast})}>
-                    <Image src={hasCompleted ? hasClaimed ? opennedChest : closeChest : lockedChest}/>
+                    <Image src={hasClaimed ? opennedChest : hasCompleted ? closeChest : lockedChest}/>
                 </Box>
             <Spacer />
             <Card level={levels[1]}/>
