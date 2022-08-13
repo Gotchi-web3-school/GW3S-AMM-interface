56f8c6e954cd0436f0296a2e74f6c2bfbffe41e6
John <bioj@hotmail.fr>
2022-08-13 15:46:44 +0900

    level2: DONE

--- src/Components/Levels/Level2.tsx
+++ src/Components/Levels/Level2.tsx
@@ -1,13 +1,121 @@
-import {Box, Stack, Spacer} from "@chakra-ui/react"
-import Card from "./Card"
+import { useContext, useEffect, useState } from "react";
+import {Box, Stack, Spacer, HStack, Button, Center, useToast, keyframes} from "@chakra-ui/react"
+import useSound from 'use-sound';
+import { LevelContext } from "../../Provider/LevelProvider";
 import { levels } from "../../Constants/levels"
+import { ApproveTx } from "../../Models";
+import { approve, fetchApprovedL2Tokens, fetchLevel2State, Level2State, ShipTokensTx, shipTokensTx } from "../../Lib/Smart-contracts/Levels/level2Facet";
+import Card from "./Card"
+import { useWeb3React } from "@web3-react/core";
+import { ContractContext } from "../../Provider/ContractProvider";
+import Kek from "../../Assets/sprites/Kek";
+import Alpha from "../../Assets/sprites/Alpha";
+import Fomo from "../../Assets/sprites/Fomo";
+import Fud from "../../Assets/sprites/Fud";
+import JohnAaveBoat from "../../Assets/sprites/JohnAaveBoat";
+const oceanSfx = require("../../Assets/sounds/oceanWave.mp3")
+
+const shipKeyFrames = keyframes `
+    from {transform: translateX(0)}
+    to {transform: translateX(100rem)} 
+`
+
+const shipAnimation = `${shipKeyFrames} 5s linear 0.5s 1 normal forwards`
 
 const Level2: React.FC = () => {
+    const signer = useWeb3React();
+    const {LevelLoupeFacet} = useContext(ContractContext);
+    const {instanceAddress, tokens, dispatch, running} = useContext(LevelContext);
+    const [play, {stop}] = useSound(oceanSfx, {volume: 0.6})
+    const toast = useToast();
+    const [shipped, setShipped] = useState(false);
+    const [loading, setLoading] = useState({KEK: false, ALPHA: false, FOMO: false, FUD: false});
+    const [approved, setApproved] = useState({KEK: false, ALPHA: false, FOMO: false, FUD: false});
+
+    // Approve one of the four tokens
+    const approveTx = (e: any) => {
+        e.preventDefault();
+        const tx: ApproveTx = {
+            signer: signer,
+            tokenAddress: tokens[parseInt(e.target.id)],
+            tokenName: e.target.name,
+            instanceAddress: instanceAddress,
+            toast: toast,
+        }
+        
+        setLoading({...loading, [`${e.target.name}`]: true})
+        console.log(loading)
+        
+        approve(tx)
+        .then(() => setApproved({...approved, [`${e.target.name}`]: true}))
+        
+        setLoading({...loading, [`${e.target.name}`]: false})
+    }
+
+    const shipTx = () => {
+        const tx: ShipTokensTx = {
+            signer: signer,
+            instanceAddress: instanceAddress,
+            toast: toast
+        }
+        shipTokensTx(tx).then(() => {
+            play()
+            setShipped(true)
+            setTimeout(stop, 7000)
+        })
+    }
+
+    // Fetch the state of the level
+    useEffect(() => {
+        try {
+            if (running === 2) {
+                setLoading({KEK: true, ALPHA: true, FOMO: true, FUD: true})
+                fetchLevel2State(signer, 2).then((result: Level2State) => {
+                    dispatch({type: "SET_LEVEL_STATE", payload: result})
+                    setShipped(result.shipped)
+                    fetchApprovedL2Tokens(result.tokens, instanceAddress, signer)
+                    .then(result => {
+                        setLoading({KEK: false, ALPHA: false, FOMO: false, FUD: false})
+                        setApproved(result)
+                    })
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }, [LevelLoupeFacet, signer, dispatch, instanceAddress, running])
+
     return (
-        <Box margin={"auto"}>
+    <Box margin={"auto"}>
         <Stack direction={"row"} m="5rem" align="center">
             <Spacer />
-
+           {running === 2 && 
+                <Stack>
+                    <Center>
+                        <Box animation={shipped ? shipAnimation : ''} position={"relative"}>
+                            <JohnAaveBoat size={"25rem"} />
+                            <HStack position={"relative"} bottom="10rem" zIndex={1} ml="2rem">
+                                {approved.KEK && <Kek size={"5rem"} />}
+                                {approved.ALPHA && <Alpha size={"5rem"} />}
+                                {approved.FOMO &&  <Fomo size={"5rem"} />}
+                                {approved.FUD && <Fud size={"5rem"} />}
+                            </HStack>
+                        </Box>
+                    </Center >
+                    <HStack justifyContent={"space-between"} >
+                        <Button id="0" name="KEK" isLoading={loading.KEK} bg={approved.KEK ? "green.300" : ""} onClick={approveTx}>Approve KEK</Button>
+                        <Button id="1" name="ALPHA" isLoading={loading.ALPHA} bg={approved.ALPHA ? "green.300" : ""} onClick={approveTx}>Approve ALPHA</Button>
+                        <Button id="2" name="FOMO" isLoading={loading.FOMO} bg={approved.FOMO ? "green.300" : ""} onClick={approveTx}>Approve FOMO</Button>
+                        <Button id="3" name="FUD" isLoading={loading.FUD} bg={approved.FUD ? "green.300" : ""} onClick={approveTx}>Approve FUD</Button>
+                    </HStack>
+                    <Button
+                    bg={shipped ? "green.300" : "whiteAlpha.500"}
+                    disabled={!approved.KEK && !approved.ALPHA && !approved.FOMO && !approved.FUD} 
+                    onClick={shipTx}>
+                        Ship alchemicas
+                    </Button>
+                </Stack>
+            }
             <Spacer />
             <Card level={levels[2]}/>
         </Stack>
--- src/Lib/Smart-contracts/Levels/level2Facet.ts
+++ src/Lib/Smart-contracts/Levels/level2Facet.ts
@@ -1,25 +1,34 @@
 import { ethers } from "ethers";
+import { CompleteTx, ClaimTx, InitTx, ApproveTx } from "../../../Models/index"
+import { interfaces } from "../../../Constants/interfaces"
+import { GlobalConst, DIAMOND_ADDRESS } from "../../../Constants";
+import { fetchApprovedToken } from "../../Utils";
 
-interface ClaimTx {
-    ILevel0: ethers.Contract | undefined,
-    toast: any,
+const REQUIRED_ALLOWANCE = 10
+
+type ApprovedL2Tokens = {
+    KEK: boolean,
+    ALPHA: boolean,
+    FOMO: boolean,
+    FUD: boolean,
 }
 
-export const claim_l0 = async(tx: ClaimTx) => {
+export const start_l2 = async(tx: InitTx) => {
     try {    
-        console.warn("CLAIM")
+        console.warn("INIT")
         console.log("///////////////////////////////////////////////")
-        console.log("Level: " + 0)
+        console.log("Level: " + 2)
         console.log("///////////////////////////////////////////////")
+
     
         //Estimation of the gas cost
-        const gas = await tx.ILevel0?.estimateGas.claim_l0() 
+        const gas = await tx.Facet?.estimateGas.initLevel2() 
         console.log("Gas cost: " + (ethers.utils.formatEther(gas?.toString() ?? "") + " MATIC"))
         
-        const transaction = await tx.ILevel0?.claim_l0()
+        const transaction = await tx.Facet?.initLevel2()
     
         tx.toast({
-            title: `Claim level 0`,
+            title: `Init level 2`,
             description: `transaction pending at: ${transaction?.hash}`,
             position: "top-right",
             status: "warning",
@@ -29,8 +38,153 @@ export const claim_l0 = async(tx: ClaimTx) => {
         const receipt = await transaction.wait()
     
         tx.toast({
-            title: `Claim level 0`,
+            title: `Init level 2`,
+            description: `level initiated successfully`,
+            position: "top-right",
+            status: "success",
+            duration: 6000,
+            isClosable: true,
+            })
+        console.log(receipt)
+
+        tx.dispatch({type: "INIT", payload: 2})
+        
+    } catch (error: any) {
+        console.log(error.error)
+        tx.toast({
+            position: "bottom-right",
+            title: 'Init level 2.',
+            description: `${error.message}`,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+    }
+}
+
+export const complete_l2 = async(tx: CompleteTx) => {
+    try {    
+        console.warn("COMPLETE")
+        console.log("///////////////////////////////////////////////")
+        console.log("Level: " + 2)
+        console.log("///////////////////////////////////////////////")
+        
+        //Estimation of the gas cost
+        const gas = await tx.Facet?.estimateGas.complete_l2() 
+        console.log("Gas cost: " + (ethers.utils.formatEther(gas?.toString() ?? "") + " MATIC"))
+        
+        const transaction = await tx.Facet?.complete_l2()
+        
+        tx.toast({
+            title: `Complete level 2`,
+            description: `transaction pending at: ${transaction?.hash}`,
+            position: "top-right",
+            status: "warning",
+            isClosable: true,
+        })
+        
+        const receipt = await transaction.wait()
+        
+        tx.toast({
+            title: `Complete level 2`,
+            description: `Level completed successfully`,
+            position: "top-right",
+            status: "success",
+            duration: 6000,
+            isClosable: true,
+            })
+            console.log(receipt)
+
+            const hasCompleted = await tx.LoupeFacet?.hasCompletedLevel(tx.signer.account, 2)
+            tx.dispatch({type: "COMPLETED", payload: hasCompleted})
+            
+        } catch (error: any) {
+            console.log(error.error)
+            tx.toast({
+                position: "bottom-right",
+                title: 'Complete level 2.',
+                description: `${error.error.message}`,
+                status: 'error',
+            duration: 9000,
+            isClosable: true,
+        })
+    }
+}
+
+export const claim_l2 = async(tx: ClaimTx) => {
+    try {    
+        console.warn("CLAIM")
+        console.log("///////////////////////////////////////////////")
+        console.log("Level: " + 2)
+        console.log("///////////////////////////////////////////////")
+
+        //Estimation of the gas cost
+        const gas = await tx.Facet?.estimateGas.claim_l2() 
+        console.log("Gas cost: " + (ethers.utils.formatEther(gas?.toString() ?? "") + " MATIC"))
+        
+        const transaction = await tx.Facet?.claim_l2()
+    
+        tx.toast({
+            title: `Claim level 2`,
+            description: `transaction pending at: ${transaction?.hash}`,
+            position: "top-right",
+            status: "warning",
+            isClosable: true,
+            })
+    
+        const receipt = await transaction.wait()
+    
+        tx.toast({
+            title: `Claim level 2`,
             description: `Reward claimed successfully`,
+            position: "top-right",
+            status: "success",
+            duration: 6000,
+            isClosable: true,
+            })
+        console.log(receipt)
+
+        tx.dispatch({type: "CLAIM", payload: true})
+        
+    } catch (error: any) {
+        console.log(error.error)
+        tx.toast({
+            position: "bottom-right",
+            title: 'Claim level 2.',
+            description: `${error.error.message}`,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+    }
+}
+
+export const approve = async(tx: ApproveTx) => {
+    const {signer, tokenAddress, tokenName, instanceAddress, toast} = tx
+    try {    
+        console.warn("APPROVE " + tokenName + "\noperator: " + instanceAddress +"\namount: " + GlobalConst.utils.MAX_INT)
+
+        const token = new ethers.Contract(tokenAddress, interfaces.IToken, signer.library.getSigner(signer.account))
+        
+        //Estimation of the gas cost
+        const gas = await token.estimateGas.approve(instanceAddress, GlobalConst.utils.MAX_INT) 
+        console.log("Gas cost: " + (ethers.utils.formatEther(gas?.toString() ?? "") + " MATIC"))
+        
+        const transaction = await token.approve(instanceAddress, GlobalConst.utils.MAX_INT)
+    
+        toast({
+            title: `Approve ${tokenName}`,
+            description: `transaction pending at: ${transaction?.hash}`,
+            position: "top-right",
+            status: "warning",
+            isClosable: true,
+            })
+    
+        const receipt = await transaction.wait()
+    
+        toast({
+            title: `Approve ${tokenName}`,
+            description: `token approved successfully`,
             position: "top-right",
             status: "success",
             duration: 6000,
@@ -39,14 +193,123 @@ export const claim_l0 = async(tx: ClaimTx) => {
         console.log(receipt)
         
     } catch (error: any) {
-        console.log(error.error)
-        tx.toast({
+        console.log(error.message)
+        toast({
             position: "bottom-right",
-            title: 'Claim level 0.',
+            title: `Approve ${tokenName}`,
             description: `${error.error.message}`,
             status: 'error',
             duration: 9000,
             isClosable: true,
           })
+        throw new Error(error.message)
+    }
+}
+
+export interface ShipTokensTx {
+    signer: any,
+    instanceAddress: string,
+    toast: any,
+}
+
+export const shipTokensTx = async(tx: ShipTokensTx) => {
+    try {
+        const {signer, instanceAddress, toast} = tx
+        console.warn("SHIP ALCHEMICAS")
+
+        const instance = new ethers.Contract(instanceAddress, interfaces.ILevel2Instance, signer.library.getSigner(signer.account))
+
+        //Estimation of the gas cost
+        const gas = await instance.estimateGas.shipTokens() 
+        console.log("Gas cost: " + (ethers.utils.formatEther(gas?.toString() ?? "") + " MATIC"))
+        
+        const transaction = await instance.shipTokens()
+    
+        toast({
+            title: `Ship tokens`,
+            description: `transaction pending at: ${transaction?.hash}`,
+            position: "top-right",
+            status: "warning",
+            isClosable: true,
+            })
+    
+        const receipt = await transaction.wait()
+    
+        toast({
+            title: `Ship tokens`,
+            description: `tokens shipped successfully`,
+            position: "top-right",
+            status: "success",
+            duration: 6000,
+            isClosable: true,
+            })
+        console.log(receipt)
+
+    } catch (error: any) {
+        console.log(error.message)
+        tx.toast({
+            position: "bottom-right",
+            title: `Ship tokens`,
+            description: `${error.error.message}`,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+        throw new Error(error.message)
+    }
+}
+
+export const fetchApprovedL2Tokens = async(tokens: string[], operator: string, signer: any): Promise<ApprovedL2Tokens> => {
+    const promises = []
+
+    for(let i = 0; i < 4; i++) {
+        promises.push(fetchApprovedToken(tokens[i], REQUIRED_ALLOWANCE, operator, signer))
+    }
+    const result = await Promise.all(promises)
+
+    return {
+        KEK: result[0], 
+        ALPHA: result[1], 
+        FOMO: result[2], 
+        FUD: result[3], 
+    }
+}
+
+export interface Level2State {
+    running: number,
+    instanceAddress: string,
+    hasCompleted: boolean,
+    hasClaimed: boolean,
+    factory: string,
+    tokens: string[],
+    shipped: boolean
+}
+
+export const fetchLevel2State = async(signer: any, id: number): Promise<Level2State> => {
+    const LevelLoupeFacet = new ethers.Contract(DIAMOND_ADDRESS, interfaces.LevelLoupeFacet, signer.library)
+    let tokens: string[] = []
+
+    const instanceAddress: string = await LevelLoupeFacet!.getLevelInstanceByAddress(signer.account, id)
+    const Level2Instance = new ethers.Contract(instanceAddress, interfaces.ILevel2Instance, signer.library)
+    
+    const running: BigInt = LevelLoupeFacet.getRunningLevel(signer.account)
+    const hasCompleted: boolean = LevelLoupeFacet.hasCompletedLevel(signer.account, id)
+    const hasClaimed: boolean = LevelLoupeFacet.hasClaimedLevel(signer.account, id)
+    const factories: string = LevelLoupeFacet.getFactoryLevel(id, 0)
+    const shipped: boolean = Level2Instance.shipped()
+    for(let i = 0; i < 4; i++) (
+        tokens.push(Level2Instance.tokens(i))
+    )
+    
+    const result = await Promise.all([running, hasCompleted, hasClaimed, factories, Promise.all(tokens), shipped])
+
+    return {
+        running: parseInt(result[0].toString()),
+        instanceAddress: instanceAddress,
+        hasCompleted: result[1],
+        hasClaimed: result[2],
+        factory: result[3],
+        tokens: result[4],
+        shipped: result[5]
     }
 }
