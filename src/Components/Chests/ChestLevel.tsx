236b9082f0f826011a7d567e1822bc5e0b796213
John <bioj@hotmail.fr>
2022-08-20 01:32:51 +0900

    update chest State type

--- src/Components/Chests/ChestLevel.tsx
+++ src/Components/Chests/ChestLevel.tsx
@@ -5,20 +5,24 @@ import { LevelContext } from "../../Provider/LevelProvider"
 import { opens } from "../../Lib/Smart-contracts/Levels"
 import ModalChestOpen from "../Modal/ModalChestOpen"
 import { OpennedChest } from "../../Models"
+import { useWeb3React } from "@web3-react/core"
+import { Reward } from "../../Lib/Smart-contracts/Rewards"
 const lockedChest = require("../../Assets/chests/lockedChest.png")
 const closeChest = require("../../Assets/chests/closedChest.png")
 const opennedChest = require("../../Assets/chests/opennedChest.png")
 
 const ChestLevel: React.FC<{id: number}> = ({id}) => {
     const toast = useToast()
+    const signer = useWeb3React()
     const { isOpen, onOpen, onClose } = useDisclosure()
-    const [chest, setChest] = useState<OpennedChest>({loots: [], amounts: []})
+    const [chest, setChest] = useState<Array<Reward | undefined>>([])
     const {LevelFacets} = useContext(ContractContext)
     const {hasCompleted, dispatch} = useContext(LevelContext)
 
     const openChest = async() => {
         try {
             const result = await opens[id]({
+                signer: signer,
                 Facet: LevelFacets[id], 
                 toast: toast,
                 dispatch: dispatch,
