3b613b97ca6b5c415ee9d93cad2d0db9f9b7c565
John <bioj@hotmail.fr>
2022-08-20 06:33:14 +0900

    add useEffect for fetching level state

--- src/Provider/LevelProvider.tsx
+++ src/Provider/LevelProvider.tsx
@@ -1,5 +1,9 @@
-import { createContext, useReducer } from "react"
+import { createContext, useReducer, useEffect, useContext } from "react"
 import { levelReducer } from "../Reducers/levelReducer"
+import { fetchLevelState } from "../Lib/Smart-contracts/Levels"
+import { ContractContext } from "./ContractProvider"
+import { useWeb3React } from "@web3-react/core"
+import { useParams } from "react-router-dom"
 
 export type LevelContextType = {
     running: number,
@@ -24,8 +28,23 @@ const defaultContext = {
 export const LevelContext = createContext<LevelContextType>(defaultContext)
 
 export const LevelProvider = (props: any) => {
-    const [level, dispatch] = useReducer(levelReducer, defaultContext)
-    const {running, instanceAddress, hasCompleted, hasClaimed, factories, tokens} = level
+  const signer = useWeb3React()
+  const { id } = useParams()
+  const {LevelLoupeFacet} = useContext(ContractContext)
+  const [level, dispatch] = useReducer(levelReducer, defaultContext)
+  const {running, instanceAddress, hasCompleted, hasClaimed, factories, tokens} = level
+
+  useEffect(() => {
+    if (LevelLoupeFacet && signer.account && id) {
+      try {
+        fetchLevelState(LevelLoupeFacet!, signer, parseInt(id)).then(result => {
+            dispatch({type: "SET_LEVEL_STATE", payload: result})
+        })
+      } catch (error) {
+          console.log(error)
+      }
+    }
+  }, [LevelLoupeFacet, signer, id, dispatch])
 
   return (
     <LevelContext.Provider value={{running, instanceAddress, hasCompleted, hasClaimed, factories, tokens, dispatch}}>
